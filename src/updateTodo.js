70568cbb2916bd8aaacd13869aed8889a554663b
Omar Cantu <ocantu@novaentertainment.com.au>
2021-10-22 21:41:18 +1100

    First commit

---
+++ src/updateTodo.js
@@ -0,0 +1,31 @@
+const AWS = require('aws-sdk');
+const middy = require('@middy/core');
+const httpJsonBodyParser = require('@middy/http-json-body-parser');
+
+const updateTodo = async (event) => {
+  const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+  const { completed } = event.body;
+  const { id } = event.pathParameters;
+
+  await dynamodb.update({
+    TableName: 'TodoTable',
+    Key: { id },
+    UpdateExpression: 'set completed = :completed',
+    ExpressionAttributeValues: {
+      ':completed': completed
+    },
+    ReturnValues: 'ALL_NEW',
+  }).promise();
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      msg: 'Todo updated'
+    }),
+  };
+};
+
+module.exports = {
+  handler: middy(updateTodo).use(httpJsonBodyParser())
+}
